1c2a708d7ae32decf41056c78428505ee7765abd
Profsain <husseinimudiking@gmail.com>
2025-10-30 11:18:05 +0100

    add card text

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const WeatherCard = () => {
   return (
-    <div>WeatherCard</div>
+    <div>
+        <h1>Card Ui</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, enim! Autem itaque quisquam, repellendus ipsa ipsum doloremque a maiores placeat laboriosam sunt nam repellat, fuga repudiandae deleniti corrupti quidem similique.</p>
+    </div>
   )
 }
 
